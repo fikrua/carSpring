0792671a7872d13ddf72aac0f40c7bfa76240471
fikrua <fikruayal@gmail.com>
2019-05-15 16:26:45 -0400

    last

---
+++ src/main/java/carspring/carspring/CarspringApplication.java
@@ -0,0 +1,13 @@
+package carspring.carspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carspring/carspring/CarspringApplicationTests.java
@@ -0,0 +1,16 @@
+package carspring.carspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
